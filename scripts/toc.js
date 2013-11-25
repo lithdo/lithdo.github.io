072813443bb823f47e21d14f3a18ad3299a8801f
lithdo <lithdo@gmail.com>
2013-11-25 23:49:10 +0800

    用h50插件（https://code.google.com/p/h5o/）来生成目录。

--- scripts/toc.js
+++ scripts/toc.js
@@ -1,29 +1,7 @@
 $(document).ready(function () {
     function updateTOC() {
-        var toc = '';
-        var old_level = 0;
-        $('h1,h2,h3,h4').each(function (i, h) {
-            h.id = 'title' + i;
-
-            if (i == 0) {
-                toc += '<p><a href="#title0">' + h.innerHTML + '</a></p>'
-                return;
-            }
-
-            level = parseInt(h.tagName.substring(1));
-            if (level < old_level) {
-                toc += '</ol>';
-            } else if (level > old_level) {
-                toc += '<ol>';
-            }
-            toc += '<li><a href=#' + h.id + '>' + h.innerHTML + '</a>';
-            old_level = level;
-        });
-        while (level > 1) {
-            toc += '</ol>';
-            level -= 1;
-        }
-
+        var outline = HTML5Outline(document.getElementById("content"));
+        var toc = outline.asHTML(true);
         $("#toc").html(toc);
     }
 
