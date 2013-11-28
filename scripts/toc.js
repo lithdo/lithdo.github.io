8717b7bdf1e76b89ef87e935113f2919586a1dfd
lithdo <lithdo@gmail.com>
2013-11-28 23:46:39 +0800

    增加回到顶部按钮
    实现方式参考http://webdesignerwall.com/tutorials/animated-scroll-to-top。

--- scripts/toc.js
+++ scripts/toc.js
@@ -11,8 +11,8 @@ $(document).ready(function () {
         $("#toc").animate({top: offsetTop}, {duration: 500, queue: false});
     });
 
-    $("body").append('<div id="tocSwitcher" class="float" onclick="$(\'#toc\').toggle(500);"></div>');
-    $("body").append('<div id="toc" class="float"></div>');
+    $("body").attr("id", "#top");
+    $("body").append('<div id="side-tools"><div id="tocSwitcher" class="float" onclick="$(\'#toc\').toggle(500);"></div><div id="toc" class="float"></div><p id="back-top"><a href="#top"><span></span>回到顶部</a></p></div>');
     updateTOC();
 
     $("#toc a").click(function () {
@@ -20,4 +20,23 @@ $(document).ready(function () {
         var pos = $(rel).offset().top;//获取该点到头部的距离
         $("html,body").animate({scrollTop: pos}, 800);
     })
+    $("#back-top").hide();
+
+    $(function() {
+        $(window).scroll(function() {
+            if($(this).scrollTop() > 100) {
+                $('#back-top').fadeIn();
+            }
+            else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+        $('#back-top a').click(function() {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
 });
